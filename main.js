c092aa8ed7a7b102a8c313955d05892e59c71153
Ryan <rkoryan88@Outlook.com>
2023-10-10 10:12:08 -0700

    Initializes repository

---
+++ main.js
@@ -0,0 +1,37 @@
+const path = require("path");
+/*
+ * Project: Milestone 1
+ * File Name: main.js
+ * Description:
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const IOhandler = require("./IOhandler");
+const zipFilePath = path.join(__dirname, "myfile.zip");
+const pathUnzipped = path.join(__dirname, "unzipped");
+const pathProcessed = path.join(__dirname, "grayscaled");
+const fs = require("fs")
+const unzipper = require("unzipper");
+
+// fs.createReadStream(zipFilePath)
+//   .pipe(unzipper.Extract({ path: "./unzipped" }));
+
+  //Read each png file...
+  //fs.createReadStream("png1.png")
+
+  //Step 1: Read the zip file
+  //Step 2: Unzip the zip file  .then should only run after ziping occufs, use promises
+  //Step 3: read all png images from unzipped folder
+  //Step 4: Send them to the grayscale filter function
+  //Step 5: After ALL Images have SUCCESSFULLY been grayscaled, (show a success message)
+  //ALL ERRORS MUST SHOW IN .catch in PROMISE CHAIN
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(("All images done!"))
+})
+
+greyScale("img1.png"), greyScale("img2.png"), greyScale("img3.png")
+.then (() => console.log("All images done!"))
