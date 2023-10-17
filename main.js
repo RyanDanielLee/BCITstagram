a4d8999e9d2a6679ce81f571630f9029f4c93b2b
Ryan <rkoryan88@outlook.com>
2023-10-16 19:54:29 -0700

    removedcomments

--- main.js
+++ main.js
@@ -31,22 +31,3 @@ IOhandler.unzip(zipFilePath, pathUnzipped)
   .then(() => console.log("All Images Done"))
   .catch(console.log(error))
 
-// fs.createReadStream(zipFilePath)
-//   .pipe(unzipper.Extract({ path: "./unzipped" }));
-
-  //Read each png file...
-  //fs.createReadStream("png1.png")
-
-  //Step 1: Read the zip file
-  //Step 2: Unzip the zip file  .then should only run after ziping occufs, use promises
-  //Step 3: read all png images from unzipped folder
-  //Step 4: Send them to the grayscale filter function
-  //Step 5: After ALL Images have SUCCESSFULLY been grayscaled, (show a success message)
-  //ALL ERRORS MUST SHOW IN .catch in PROMISE CHAIN
-
-// Promise.all([promise1, promise2, promise3]).then((values) => {
-//     console.log(("All images done!"))
-// })
-
-// greyScale("img1.png"), greyScale("img2.png"), greyScale("img3.png")
-// .then (() => console.log("All images done!"))
