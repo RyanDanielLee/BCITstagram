fe9219c192200ab9b2f45b3fdc08d96de37f6295
Ryan <rkoryan88@outlook.com>
2023-10-16 21:03:52 -0700

    Adds more imports

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ const path = require("path");
  *
  */
 
+const path = require("path");
 const IOhandler = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
+const pathProcessed = path.join(__dirname, "grayscaled");
 
 IOhandler.unzip(zipFilePath, pathUnzipped)
   .then(() => IOhandler.readDir(pathUnzipped))
